c1d9d11f2b229a184472df0595fd79e7c0448985
Sikandar Hashmi <sikandar.salman@gmail.com>
2025-10-08 19:12:18 -0400

    Set production API base URL to Render endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // This file stores the API URL, the key to connecting frontend to backend.
 
 // The port for your local Express API is 5000
-const API_BASE_URL = 'http://localhost:5000'; 
+const API_BASE_URL = 'https://battutah-blog-api.onrender.com'; 
 
 export default API_BASE_URL;
