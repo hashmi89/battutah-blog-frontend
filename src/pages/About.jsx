48789db1b93214333f3fd29a7ccf0be43f6e31ed
Sikandar Hashmi <sikandar.salman@gmail.com>
2025-11-10 08:51:17 -0500

    Add bootstrap styling to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,11 @@
 export function About() {
-  
+
   return (
-    <>
-      <div>
-        Travel
+      <div class="p-5 text-center bg-body-tertiary rounded-3">
+        <h2 class="text-body-emphasis">Travel</h2> 
+        <h2 class="text-body-emphasis">Eat</h2>
+        <h2 class="text-body-emphasis">Sleep</h2>
+        <h1 className="text-body-emphasis">Repeat</h1>
       </div>
-      <div>
-        Eat
-      </div>
-      <div>
-        Sleep
-      </div>
-      <div>
-        Repeat
-      </div>
-    </>
   );
 }
